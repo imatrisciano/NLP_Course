4a936d6b00d2efa353c3b67d63d12c21d21e660c
ivano <ivanomatrisciano@gmail.com>
2023-03-30 18:06:28 +0200

    Practice3: Added feature scaling, this improved accuracy and f1

--- Solutions/Practice3/Solution3/main.py
+++ Solutions/Practice3/Solution3/main.py
@@ -4,19 +4,18 @@ from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_sc
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.preprocessing import StandardScaler
 
 from dataset_reader import read_dataset
 from EmbeddingDatasetReader import *
 from normalization import MyNormalizer
 from ngrams import *
 
-
 itwac_path = '../../../data/word2vec/itwac128.sqlite'
 twitter_path = '../../../data/word2vec/twitter128.sqlite'
 haspedee_dataset_path = '../../../data/hate_speech/haspeede2020/haspeede2_dev_taskAB.tsv'
-#haspedee_test_dataset_path = '../../../data/hate_speech/haspeede2020/haspeede2_test_taskAB-tweets.tsv'
+# haspedee_test_dataset_path = '../../../data/hate_speech/haspeede2020/haspeede2_test_taskAB-tweets.tsv'
 id_to_label = {"0": "NoHate", "1": "Hate"}
-
 
 embedding_size = 128
 
@@ -37,10 +36,19 @@ def customFilter(words: list) -> list:
     blacklist = ["user", "url"]
     return [x for x in words if x not in blacklist and not contains_digits(x)]
 
+
 def contains_digits(s):
     return any(char.isdigit() for char in s)
 
+
 def svc(x, y, cv=5):
+    """
+    This piece of code was written in collaboration with github.com/alessandroquirile/ for another project
+    :param x: training data
+    :param y: labels
+    :param cv: number of cross validation folds
+    :return: best_model, metrics
+    """
 
     param_grid = {'C': [0.01, 0.1, 1, 10],
                   'kernel': ['linear', 'rbf', 'poly'],
@@ -55,7 +63,8 @@ def svc(x, y, cv=5):
     best_model_metrics = results.iloc[[grid.best_index_]]  # select only the row of the best classifier
     return best_model, best_model_metrics
 
-def extractDocumentFeatures(words:list) -> np.array:
+
+def extractDocumentFeatures(words: list) -> np.array:
     """
     # By executing this we can observe that capitalization does matter in the embeddings dataset as the embedding
     # vectors of 'Che' and 'che' are different
@@ -74,14 +83,13 @@ def extractDocumentFeatures(words:list) -> np.array:
 
     return embeddingSum
 
+
 if __name__ == "__main__":
     data_path = twitter_path
 
     # Reading embeddings
     # Each row contains a word and the corresponding embedding (128 dimensions)
     embeddings = EmbeddingDatasetReader(data_path)
-
-
 
     documents, labels, text_to_id_map = read_dataset(haspedee_dataset_path)
     train_documents, test_documents, train_labels, test_labels = train_test_split(documents, labels, test_size=0.1)
@@ -91,35 +99,26 @@ if __name__ == "__main__":
     for doc in train_documents:
         train_features.append(extractDocumentFeatures(doc))
 
-    classifier, metrics = svc(train_features, train_labels)
+    scaler = StandardScaler()  # define the scaler
+    scaler.fit(train_features)  # fit on the training dataset
 
+    train_features = scaler.transform(train_features)
+
+    classifier, metrics = svc(train_features, train_labels)
 
     test_features = []
     for doc in test_documents:
         test_features.append(extractDocumentFeatures(doc))
+
+    test_features = scaler.transform(test_features)
 
     predicted_labels = classifier.predict(test_features)
     evaluate(test_labels, predicted_labels)
 
     """
     Output:
-    accuracy: 0.7485380116959064
-    precision: 0.6863468634686347
-    recall: 0.6813186813186813
-    f1: 0.6838235294117647
+    accuracy: 0.77046783625731
+    precision: 0.7050359712230215
+    recall: 0.7232472324723247
+    f1: 0.7140255009107468
     """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
