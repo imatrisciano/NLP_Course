cd91f2df223eca8d8bdd861533a285aefbbfc037
ivano <ivanomatrisciano@gmail.com>
2023-04-18 17:46:41 +0200

    Practice3: Added BERT solution

---
+++ Solutions/Practice3/Solution4-BERT/main.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.preprocessing import StandardScaler
+
+from dataset_reader import read_dataset
+from normalization import MyNormalizer
+
+from sentence_transformers import SentenceTransformer
+from neural_network import train_deep_neural_network
+
+model_name = "nickprock/sentence-bert-base-italian-uncased" # https://huggingface.co/nickprock/sentence-bert-base-italian-uncased
+
+
+
+haspedee_dataset_path = '../../../data/hate_speech/haspeede2020/haspeede2_dev_taskAB.tsv'
+# haspedee_test_dataset_path = '../../../data/hate_speech/haspeede2020/haspeede2_test_taskAB-tweets.tsv'
+id_to_label = {"0": "NoHate", "1": "Hate"}
+
+
+def evaluate(y_ref, y_predicted):
+    a = accuracy_score(y_ref, y_predicted)
+    p = precision_score(y_ref, y_predicted)
+    r = recall_score(y_ref, y_predicted)
+    f1 = f1_score(y_ref, y_predicted)
+
+    print("Metrics con test set:")
+    print(f" accuracy: {a}")
+    print(f" precision: {p}")
+    print(f" recall: {r}")
+    print(f" f1: {f1}")
+
+
+def customFilter(words: list) -> list:
+    blacklist = ["user", "url"]
+    return [x for x in words if x not in blacklist and not contains_digits(x)]
+
+
+def contains_digits(s):
+    return any(char.isdigit() for char in s)
+
+
+def svc(x, y, cv=5) -> (svm.SVC, pd.DataFrame):
+    """
+    This piece of code was written in collaboration with github.com/alessandroquirile/ for another project
+    :param x: training data
+    :param y: labels
+    :param cv: number of cross validation folds
+    :return: best_model, metrics
+    """
+
+    param_grid = {'C': [0.01, 0.1, 1, 10],
+                  'kernel': ['linear', 'rbf', 'poly'],
+                  'gamma': ['auto']}
+    grid = GridSearchCV(svm.SVC(), param_grid, cv=cv, n_jobs=-1, verbose=0,
+                        scoring=("accuracy", "precision", "recall", "f1"),
+                        refit="f1")
+    grid.fit(x, y)
+
+    best_model = grid.best_estimator_
+    results = pd.DataFrame(grid.cv_results_)  # results contains all the metrics for every parameter combination
+    best_model_metrics = results.iloc[[grid.best_index_]]  # select only the row of the best classifier
+    return best_model, best_model_metrics
+
+
+if __name__ == "__main__":
+    print("Loading transformer model...")
+    import torch_directml
+    directML = torch_directml.device()
+    bert_model = SentenceTransformer(model_name, device=directML)
+
+    print("Loading dataset...")
+    documents, labels, text_to_id_map = read_dataset(haspedee_dataset_path)
+    train_documents, test_documents, train_labels, test_labels = train_test_split(documents, labels, test_size=0.1)
+    wordNormalizer = MyNormalizer(language="italian")
+
+    print("Extracting training features...")
+    train_features = bert_model.encode(train_documents)
+
+    print("Fitting features scaler...")
+    scaler = StandardScaler()  # define the scaler
+    scaler.fit(train_features)  # fit on the training dataset
+
+    train_features = scaler.transform(train_features)
+
+    print("Training classification model...")
+    # classifier, metrics = svc(train_features, train_labels)
+    classifier, metrics = train_deep_neural_network(train_features, train_labels, 768)
+
+    print(f" > Best classifier is {classifier}")
+    print(metrics)
+
+    print("Extracting and scaling test features...")
+    test_features = bert_model.encode(test_documents)
+    test_features = scaler.transform(test_features)
+
+    print("Predicting test data...")
+    predicted_labels = classifier.predict(test_features)
+    evaluate(test_labels, predicted_labels)
+
+    """
+    Output con SVC:
+        Metrics con test set:
+        accuracy: 0.7660818713450293
+        precision: 0.7163636363636363
+        recall: 0.7060931899641577
+        f1: 0.7111913357400721
+    Output con Deep Neaural Network:
+        accuracy: 0.7953216374269005
+        precision: 0.7253521126760564
+        recall: 0.7686567164179104
+        f1: 0.7463768115942029
+    """
---
+++ Solutions/Practice3/Solution4-BERT/neural_network.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from keras import layers
+from keras.wrappers.scikit_learn import KerasClassifier
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
+from tensorflow import keras
+from sklearn.model_selection import cross_validate
+
+
+def create_model(input_size):
+    model = keras.Sequential(
+        [
+            layers.Dense(512, input_dim=input_size, kernel_initializer='normal', activation='relu'),
+            layers.Dropout(0.1),
+            layers.Dense(128, activation='relu'),
+            layers.Dropout(0.1),
+            layers.Dense(16, activation='relu'),
+            layers.Dense(1, activation='sigmoid')
+        ]
+    )
+
+    model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
+    return model
+def train_deep_neural_network(x_train, y_train, input_size, plot=False):
+    params = {"input_size": input_size}
+    model = KerasClassifier(build_fn=create_model, **params)
+    param_grid = {"epochs": [256], "batch_size": [8192]}
+    grid = GridSearchCV(model, param_grid, cv=5,
+                        scoring=("accuracy", "precision", "recall", "f1"),
+                        refit="f1")
+    grid.fit(x_train, y_train)
+
+    best_model = grid.best_estimator_
+    results = pd.DataFrame(grid.cv_results_)  # results contains all the metrics for every parameter combination
+    best_model_metrics = results.iloc[[grid.best_index_]]  # select only the row of the best classifier
+
+
+    """
+    if plot:
+        print(model.summary())
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title("Model's Training & Validation loss across epochs")
+        plt.ylabel('Loss')
+        plt.xlabel('Epochs')
+        plt.legend(['Train', 'Validation'], loc='upper right')
+        plt.show()
+    """
+
+    return best_model, best_model_metrics
